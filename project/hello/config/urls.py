ce896d83cb2fd4c0c3b0d780059033b95b949e0f
Francisco Colina <franco.collina@gmail.com>
2023-04-08 14:40:14 +0000

    Adding Docker

--- project/hello/config/urls.py
+++ project/hello/config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('pages.url')), # NEW 
+    path('', include('pages.urls')), # NEW 
 ]
